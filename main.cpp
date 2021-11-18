a30572a3f3cc0d0dddb03d0c152d433e86424f79
Pavlo Koval <koval.pavlo03@gmail.com>
2021-11-18 22:26:49 +0200

    Created bot

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <tgbot/tgbot.h>
+
+int main() {
+    TgBot::Bot bot("2105003709:AAFfMrnxPzvKQG8X0xYnVTwyiMQVIeg5v60");
+    
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message)
+    {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            printf("Wrote start");
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true)
+        {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
